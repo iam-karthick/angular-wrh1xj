427af7aa807b1cef1474f75f295456322491dc86
iam-karthick <dkarthickeee12@gmail.com>
2019-06-22 12:31:12 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import {FormControl,FormGroup} from '@angular/forms'
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'hola';
+  myFormControl:FormControl;
+  constructor(){
+    this.myFormControl = new FormControl("Karthi")
+  }
 }
